8e90a8bf7f91d3991326877d552784122d2833a7
Greg Rickaby <gregrickaby@gmail.com>
2021-02-14 15:12:03 -0600

    Add <Header />

---
+++ components/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header(): JSX.Element {
+  return (
+    <header>
+      <div className="header-content">
+        <h1>Crochet Bernie&apos;s Mittens Doll</h1>
+      </div>
+    </header>
+  )
+}
