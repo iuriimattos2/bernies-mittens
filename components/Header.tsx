21746808036586f76a86b2ec21e63b8097e7df71
Greg Rickaby <gregrickaby@gmail.com>
2021-02-14 16:04:24 -0600

    style header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,9 @@
+import {SITE_NAME} from '../config/index'
+
 export default function Header(): JSX.Element {
   return (
     <header>
-      <div className="header-content">
-        <h1>Crochet Bernie&apos;s Mittens Doll</h1>
-      </div>
+      <h1 className="text-sm text-gray-900 font-medium">{SITE_NAME}</h1>
     </header>
   )
 }
