2d88e4889d00364045b009080e57227c4163828a
Greg Rickaby <gregrickaby@gmail.com>
2021-02-14 15:58:05 -0600

    add tailwind

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
-import { AppProps } from 'next/app'
-import '../styles.css'
+import {AppProps} from 'next/app'
+import 'tailwindcss/tailwind.css'
+import 'styles/global.css'
 
 function MyApp({Component, pageProps}: AppProps): JSX.Element {
   return <Component {...pageProps} />
