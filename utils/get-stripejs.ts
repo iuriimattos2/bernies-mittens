612bdaf955c16b428500d19654f071d1fc0b8cbe
Greg Rickaby <gregrickaby@gmail.com>
2021-02-14 15:31:12 -0600

    add return type

--- utils/get-stripejs.ts
+++ utils/get-stripejs.ts
@@ -1,7 +1,7 @@
 import { loadStripe, Stripe } from '@stripe/stripe-js'
 
 let stripePromise: Promise<Stripe | null>
-export default function getStripe() {
+export default function getStripe(): Promise<Stripe | null> {
   if (!stripePromise) {
     stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!)
   }
