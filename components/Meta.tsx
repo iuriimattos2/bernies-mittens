1094a4c09702a94828786f8a02faa74e894033a7
Greg Rickaby <gregrickaby@gmail.com>
2021-02-14 15:12:10 -0600

    add <Meta />

---
+++ components/Meta.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+type Props = {
+  description?: string
+  title?: string
+}
+
+export default function Meta({title, description}: Props): JSX.Element {
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta name="description" content={description} />
+    </Head>
+  )
+}
