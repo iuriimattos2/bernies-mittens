9535603e26db95713555075f0e3d2fdff333e864
Greg Rickaby <gregrickaby@gmail.com>
2021-02-14 16:23:46 -0600

    style footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,3 +1,10 @@
 export default function Footer(): JSX.Element {
-  return <p>&copy; 2021 Crochet Creations by Tara Rickaby</p>
+  return (
+    <footer>
+      <p className="text-sm text-gray-900 font-medium">
+        &copy; 2021 Crochet Creations by{' '}
+        <a href="https://tararickaby.com">Tara Rickaby</a>
+      </p>
+    </footer>
+  )
 }
