37a8169ee2ed67467c36f26ed6923680fa73870c
Greg Rickaby <gregrickaby@gmail.com>
2021-02-15 11:46:25 -0600

    add link to itself

--- components/Product.tsx
+++ components/Product.tsx
@@ -1,11 +1,14 @@
 import product from '@/data/product.json'
 import Image from 'next/image'
+import Link from 'next/link'
 import {formatCurrencyString} from 'use-shopping-cart'
 
 export default function Product(): JSX.Element {
   return (
-    <section className="text-center space-y-2 max-w-lg">
-      <h2 className="text-gray-400 text-3xl">{product.name}</h2>
+    <section className="text-center space-y-2 w-80">
+      <h2 className="text-gray-400 text-3xl">
+        <Link href="/">{product.name}</Link>
+      </h2>
       <p className="text-gray-500 text-3xl">
         {formatCurrencyString({
           value: product.price,
