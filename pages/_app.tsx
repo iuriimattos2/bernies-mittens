ec8b10c29d1b30c11750d38b8be00ad8f03aaa7d
Greg Rickaby <gregrickaby@gmail.com>
2021-02-14 21:39:14 -0600

    add <CartProvider />

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,19 @@
+import * as config from '@/config/index'
+import getStripe from '@/utils/get-stripejs'
 import {AppProps} from 'next/app'
+import React from 'react'
 import 'tailwindcss/tailwind.css'
 import 'styles/global.css'
+import {CartProvider} from 'use-shopping-cart'
 
-function MyApp({Component, pageProps}: AppProps): JSX.Element {
-  return <Component {...pageProps} />
+export default function MyApp({Component, pageProps}: AppProps): JSX.Element {
+  return (
+    <CartProvider
+      mode="checkout-session"
+      stripe={getStripe()}
+      currency={config.CURRENCY}
+    >
+      <Component {...pageProps} />
+    </CartProvider>
+  )
 }
-
-export default MyApp
