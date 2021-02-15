fbfdda8aaa6ab125756b054f1cd003c0a35606ee
Greg Rickaby <gregrickaby@gmail.com>
2021-02-15 12:34:42 -0600

    add sharing to success

--- components/Success.tsx
+++ components/Success.tsx
@@ -1,3 +1,5 @@
+import {SITE_URL} from '@/config/index'
+
 type Props = {
   data?: object
 }
@@ -5,12 +7,8 @@ type Props = {
 export default function Success(data: Props): JSX.Element {
   return (
     <div className="space-y-4">
-      <h2 className="text-3xl">Your Order Has Been Placed</h2>
-      <p>
-        Thank you <span className="capitalize">{data?.shipping?.name}</span>! A
-        receipt has been emailed to {data?.customer_details?.email}.
-      </p>
-
+      <h2 className="text-purple-400 text-3xl">Payment Successful</h2>
+      <p>A receipt has been emailed to {data?.customer_details?.email}.</p>
       <details>
         <summary>
           <strong>Shipping Details</strong>
@@ -25,9 +23,7 @@ export default function Success(data: Props): JSX.Element {
           </dt>
         </dl>
       </details>
-
       <p>Since each item is hand made, please allow 2 weeks for shipping.</p>
-
       <p>
         While you wait, check out even more plushies from{' '}
         <a className="underline" href="https://tararickaby.com">
@@ -35,6 +31,28 @@ export default function Success(data: Props): JSX.Element {
         </a>
         .
       </p>
+      <p>
+        Thank you <span className="capitalize">{data?.shipping?.name}</span>!
+      </p>
+      <div className="space-x-4">
+        <a
+          target="_blank"
+          rel="noreferrer"
+          className="share tweet"
+          href="https://twitter.com/intent/tweet?text=I+just+bought+a+Bernie%27s+Mittens+Doll+from+Crochet+Creations!"
+        >
+          Tweet
+        </a>
+
+        <a
+          target="_blank"
+          rel="noreferrer"
+          className="share facebook"
+          href={`http://www.facebook.com/dialog/send?app_id=123456789&amp;link=${SITE_URL}&amp;redirect_uri=${SITE_URL}`}
+        >
+          Facebook
+        </a>
+      </div>
     </div>
   )
 }
