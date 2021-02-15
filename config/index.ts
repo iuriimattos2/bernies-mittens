4515ea948f3af153602b5f60bee1ebeb6a545b6c
Greg Rickaby <gregrickaby@gmail.com>
2021-02-15 12:52:46 -0600

    re-add constants

--- config/index.ts
+++ config/index.ts
@@ -2,3 +2,6 @@ export const CURRENCY = 'usd'
 export const SITE_NAME = "Crochet Creations - Bernie's Mittens Doll"
 export const SITE_DESCRIPTION = "America's favorite grumpy senator is ready for a busy day running to the post office, attending Joe's inauguration, and saving our democracy in his stylish green jacket, up-cycled mittens, and mask!"
 export const SITE_URL = 'https://bernies-mittens.vercel.app/'
+export const MIN_AMOUNT = 10.0
+export const MAX_AMOUNT = 5000.0
+export const AMOUNT_STEP = 1.0
