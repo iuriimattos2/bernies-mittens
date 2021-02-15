b49a276aabca9473c773cc6a28777cbf11305cdd
Greg Rickaby <gregrickaby@gmail.com>
2021-02-15 13:56:48 -0600

    add open graph meta

--- components/Meta.tsx
+++ components/Meta.tsx
@@ -1,3 +1,4 @@
+import {OG_IMAGE, SITE_URL} from '@/config/index'
 import Head from 'next/head'
 
 interface Props {
@@ -12,6 +13,18 @@ export default function Meta({title, description}: Props): JSX.Element {
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <meta name="description" content={description} />
+
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content={SITE_URL} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={OG_IMAGE} />
+
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content={SITE_URL} />
+      <meta property="twitter:title" content={title} />
+      <meta property="twitter:description" content={description} />
+      <meta property="twitter:image" content={OG_IMAGE} />
     </Head>
   )
 }
