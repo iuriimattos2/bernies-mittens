5cb5027ff8e6c3f207c1c6b18b9cd178e7e229e1
Greg Rickaby <gregrickaby@gmail.com>
2021-02-14 21:38:00 -0600

    moar colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,13 @@ module.exports = {
       colors: {
         gray: {
           200: '#f5f4f4',
+          300: '#1a1a1a80',
           400: '#1a1a1a99',
           500: '#1a1a1ae6',
           900: '#111'
+        },
+        purple: {
+          500: '#8e66a3'
         }
       }
     }
