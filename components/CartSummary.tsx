65fb807707b7d0c8dde79a3a7076771a8377539d
Greg Rickaby <gregrickaby@gmail.com>
2021-02-15 10:06:18 -0600

    add loading animation

--- components/CartSummary.tsx
+++ components/CartSummary.tsx
@@ -41,8 +41,21 @@ export default function CartSummary(): JSX.Element {
       <div className="flex flex-col items-center space-y-8 h-48 w-64">
         <button onClick={() => addItem(product)}>Add to cart</button>
         <form className="w-full" onSubmit={handleCheckout}>
-          <button type="submit" disabled={cartEmpty || loading}>
+          <button
+            className="flex justify-center items-center"
+            type="submit"
+            disabled={cartEmpty}
+          >
             Checkout {formattedTotalPrice}
+            {loading && (
+              <svg
+                className="loading ml-2"
+                viewBox="0 0 100 100"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <circle cx="50" cy="50" r="45" />
+              </svg>
+            )}
           </button>
         </form>
         {!cartEmpty && (
