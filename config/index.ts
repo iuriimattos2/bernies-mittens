77b0a39cbd26b33bc8f899c360a8718507b567aa
Greg Rickaby <gregrickaby@gmail.com>
2021-02-15 20:51:37 -0600

    update site name

--- config/index.ts
+++ config/index.ts
@@ -1,5 +1,5 @@
 export const CURRENCY = 'usd'
-export const SITE_NAME = "Crochet Creations - Bernie's Mittens Doll"
+export const SITE_NAME = "Bernie's Mittens Doll by Crochet Creations"
 export const SITE_DESCRIPTION = "America's favorite grumpy senator is ready for a busy day running to the post office, attending Joe's inauguration, and saving our democracy in his stylish green jacket, up-cycled mittens, and mask!"
 export const SITE_URL = 'https://bernsmittens.com'
 export const OG_IMAGE = '/bernies-mittens-social.jpg'
